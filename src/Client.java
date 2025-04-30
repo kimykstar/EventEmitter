3e0904eb54039ce5e217a3e461cdb073017593e3
Younggwan <kimyogun78@naver.com>
2025-04-30 17:44:20 +0900

    feat: Event Framework

---
+++ src/Client.java
@@ -0,0 +1,11 @@
+import annotations.Emitter;
+import annotations.Event;
+
+@Emitter
+public class Client {
+
+    @Event("receive")
+    public void receive() {
+        System.out.println("client receive message");
+    }
+}
