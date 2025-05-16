a28585bb22264dd1fbe27962cd2b7d931884d155
김영관 <kimyogun78@naver.com>
2025-05-17 02:30:28 +0900

    chore: 패키지 구조 수정

--- src/components/core/EventProgramEnvorinment.java
+++ src/components/core/EventProgramEnvorinment.java
@@ -1,16 +1,20 @@
-package components;
+package components.core;
 
 
 import annotations.Event;
 
+import java.io.File;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class EventProgramEnvorinment {
 
     public static void init(EmitterManager manager) {
         Iterator<Object> it = manager.iterator();
-
+        File dir = new File("src/events");
+        File[] files = dir.listFiles();
+        System.out.println(Arrays.toString(files));
         while (it.hasNext()) {
             Object emitterInstance = it.next();
             EventEmitter emitter = manager.getEmitter(emitterInstance); // 애너테이션 찾기
