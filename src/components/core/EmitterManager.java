a96cf0be4d2eba98790aa4f0d7fdeaea81f3529a
김영관 <kimyogun78@naver.com>
2025-05-26 01:03:00 +0900

    feat: EmitterManager클래스 수정 - ClassPathScanner로 부터 받은 객체들을 바탕으로 emitters 초기화 로직 구현

--- src/components/core/EmitterManager.java
+++ src/components/core/EmitterManager.java
@@ -1,34 +1,57 @@
 package components.core;
 
 import annotations.Emitter;
+import annotations.Event;
+import components.bean.ClassPathScanner;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class EmitterManager {
-    Map<Object, EventEmitter> emitters = new HashMap<>();
+    Map<String, EventEmitter> emitters = new HashMap<>();
+    Map<String, Object> instances = new HashMap<>();
 
     public void addEmitter(Object obj) {
         Class<?> clazz = obj.getClass();
         if (!clazz.isAnnotationPresent(Emitter.class)) return;
-        emitters.put(obj, new EventEmitter());
+        String className = clazz.getName().split("\\.")[1];
+        emitters.put(className, new EventEmitter());
+        instances.put(className, obj);
     }
 
     public void removeEmitter(String name) {
         emitters.remove(name);
     }
 
-    public void emit(Object obj, String eventName) throws InvocationTargetException, IllegalAccessException {
-        EventEmitter emitter = emitters.get(obj);
-        emitter.emit(obj, eventName);
+    public void emit(String emitterName, String eventName) throws InvocationTargetException, IllegalAccessException {
+        EventEmitter emitter = emitters.get(emitterName);
+        emitter.emit(instances.get(emitterName), eventName);
     }
 
-    // emitter들을 반환한다.
-    public Iterator<Object> iterator() {
+    public Iterator<String> iterator() {
         return emitters.keySet().iterator();
     }
 
-    public EventEmitter getEmitter(Object obj) {
-        return emitters.get(obj);
+    public void initManager(ClassPathScanner classScanner) {
+        Object[] objs = classScanner.getEventInstances();
+        for (Object obj : objs) {
+            addEmitter(obj);
+        }
+
+        Iterator<String> it = iterator();
+
+        while (it.hasNext()) {
+            String emitterName = it.next();
+            EventEmitter emitter = emitters.get(emitterName);
+            Object obj = instances.get(emitterName);
+            Class<?> clazz = obj.getClass();
+            for (Method method : clazz.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(Event.class)) {
+                    String eventName = method.getAnnotation(Event.class).value();
+                    emitter.on(eventName, method);
+                }
+            }
+        }
     }
 }
